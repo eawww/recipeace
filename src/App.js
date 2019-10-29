4acc030298f999fd7ebb799e3b776cc27112a373
jtroshynski <jtroshynski@gmail.com>
2019-10-29 18:37:26 -0500

    Added basic routing to null components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        This is a thing
-      </header>
-    </div>
+    <Router>
+      <Route
+        path="/"
+        exact={true}
+        render={() => (
+          <div className="App">
+            <header className="App-header">We're HOME</header>
+            <Link to={"/display"}>
+              <h1>Display</h1>
+            </Link>
+            <Link to={"/controller"}>
+              <h1>Controller</h1>
+            </Link>
+          </div>
+        )}
+      />
+      <Route path="/controller" component={null} />
+      <Route path="/display" component={null} />
+    </Router>
   );
 }
 
