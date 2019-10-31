61c0d96b0a852cad92deef30d7bfad80ced6d7c4
Eric Wilson <eawww0@gmail.com>
2019-10-31 18:50:58 -0500

    ⛪️ Convert Controller component to functional from classinine

--- src/components/Controller.jsx
+++ src/components/Controller.jsx
@@ -1,17 +1,15 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
-class Controller extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Controller!</h1>
-        <Link to={"/"}>
-          <h2>Home</h2>
-        </Link>
-      </div>
-    );
-  }
-}
+const Controller = () => {
+  return (
+    <div>
+      <h1>Controller!</h1>
+      <Link to={"/"}>
+        <h2>Home</h2>
+      </Link>
+    </div>
+  );
+};
 
 export default Controller;
