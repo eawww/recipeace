ab0aff2a2464ac8e82d45308f04c5548e4403b0f
Eric Wilson <eawww0@gmail.com>
2019-10-31 19:18:15 -0500

    🍼 Workin on it

--- src/components/Controller.jsx
+++ src/components/Controller.jsx
@@ -1,13 +1,23 @@
-import React, { Component } from "react";
+import React, { useEffect, useState, useRef } from "react";
+import socketIOClient from "socket.io-client";
 import { Link } from "react-router-dom";
 
 const Controller = () => {
+  const socketRef = useRef();
+  useEffect(() => {
+    socketRef.current = socketIOClient("/controller");
+  }, []);
+  const sendIt = e => {
+    socketRef.current.emit(e.target.value);
+  };
   return (
     <div>
       <h1>Controller!</h1>
       <Link to={"/"}>
         <h2>Home</h2>
       </Link>
+      <input type="text" ref={textRef} />
+      <button onClick={sendIt} />
     </div>
   );
 };
